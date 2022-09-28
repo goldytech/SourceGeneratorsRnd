5c4031908dc15ac16235867fbb6f2fc81e06d3bc
goldytech <afzal.qureshi@outlook.com>
2022-09-28 17:41:31 +0530

    Deleted GeneratorConsumer project

--- GeneratorConsumer/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using GeneratorConsumer;
-
-
-var helloFromConsumer = new HelloFromConsumer();
-Console.WriteLine(helloFromConsumer.SayHelloWithNameWrapper("John"));
-Console.WriteLine(MyEnum.First.ToStringFast());
-
